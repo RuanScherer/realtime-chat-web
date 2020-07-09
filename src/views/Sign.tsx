9f56047c81900678a77a1c6d8d4798b35763e9fe
Ruan Scherer <ruan.vscherer@gmail.com>
2020-07-09 09:48:23 -0300

    Component migrated to function using hooks

--- src/views/Sign.tsx
+++ src/views/Sign.tsx
@@ -1,65 +1,54 @@
-import * as React from 'react'
+import React, { useState } from 'react'
+import  { useHistory } from 'react-router-dom'
 import ArrowRight from '../assets/ArrowRight'
 
-export interface Props {}
+const Sign: React.FC = () => {
+	const [ username, setUsername ] = useState("")
+	const [ error, setError ] = useState<boolean | null>(null)
+	const history = useHistory()
 
-export interface State {
-	username: string
-	error: boolean | null
-}
-
-export default class Sign extends React.Component<Props, State> {
-	constructor(props: Props) {
-		super(props)
-		this.state = {
-			username: "",
-			error: null
-		}
-	}
-
-	render() {
-		const { error } = this.state
-
-		return(
-			<div className="min-100vh p-2 d-flex flex-column justify-content-center align-items-center">
-				<div className="card main-card w-100 radius shadow">
-					<div className="card-body">
-						<h2 className="mb-0">Realtime Chat</h2>
-						<h5 className="text-muted mb-3">É necessário informar seu nome para continuar</h5>
-						<form >
-							<div className="form-group">
-								<label htmlFor="name" className="text-500">Nome</label>
-								<input 
-									type="text"
-									id="name"
-									name="username"
-									className={`form-control form-control-lg ${error ? "border-danger" : ""}`}
-									placeholder="Seu nome aqui"
-									onChange={evt => this.handleChangeUsername(evt.target.value)}/>
-							</div>
-							<button 
-								className="btn btn-lg btn-brown text-500 d-flex align-items-center"
-								disabled={error || error === null ? true : false}
-								onClick={evt => this.handleEnter(evt)}>
-								Começar
-								<ArrowRight />
-							</button>
-						</form>
-					</div>
-				</div>
-			</div>
-		)
-	}
-
-	handleChangeUsername = (username: string) => {
+	const handleChangeUsername = (username: string) => {
 		let error;
 		!username.length ? error = true : error = false
-		this.setState({ username, error })
+		setUsername(username)
+		setError(error)
 	}
 
-	handleEnter = (evt: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+	const handleEnter = (evt: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
 		evt.preventDefault()
-		localStorage.setItem("username", this.state.username)
-		window.location.replace("/chat")
+		localStorage.setItem("username", username)
+		history.replace("/chat")
 	}
+
+	return(
+		<div className="min-100vh p-2 d-flex flex-column justify-content-center align-items-center">
+			<div className="card main-card w-100 radius shadow">
+				<div className="card-body">
+					<h2 className="mb-0">Realtime Chat</h2>
+					<h5 className="text-muted mb-3">É necessário informar seu nome para continuar</h5>
+					<form >
+						<div className="form-group">
+							<label htmlFor="name" className="text-500">Nome</label>
+							<input 
+								type="text"
+								id="name"
+								name="username"
+								className={`form-control form-control-lg ${error ? "border-danger" : ""}`}
+								placeholder="Seu nome aqui"
+								onChange={evt => handleChangeUsername(evt.target.value)}/>
+						</div>
+						<button 
+							className="btn btn-lg btn-brown text-500 d-flex align-items-center"
+							disabled={error || error === null ? true : false}
+							onClick={evt => handleEnter(evt)}>
+							Começar
+							<ArrowRight />
+						</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	)
 }
+
+export default Sign
